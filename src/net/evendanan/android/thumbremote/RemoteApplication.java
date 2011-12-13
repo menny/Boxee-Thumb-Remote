8755c318429cb9e01d07abaa448185cec039c6ba
Menny Even Danan <menny@evendanan.net>
2011-12-14 00:04:56 +0200

    version

--- src/net/evendanan/android/thumbremote/RemoteApplication.java
+++ src/net/evendanan/android/thumbremote/RemoteApplication.java
@@ -1,7 +1,6 @@
 package net.evendanan.android.thumbremote;
 
 import android.app.Application;
-import android.os.StrictMode;
 
 public class RemoteApplication extends Application {
 
@@ -9,15 +8,15 @@ public class RemoteApplication extends Application {
 	
 	@Override
 	public void onCreate() {
-		StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
-        .detectAll()
-        .penaltyLog()
-        .build());
-		StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
-        .detectAll()
-        .penaltyLog()
-        .penaltyDeath()
-        .build());
+//		StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+//        .detectAll()
+//        .penaltyLog()
+//        .build());
+//		StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+//        .detectAll()
+//        .penaltyLog()
+//        .penaltyDeath()
+//        .build());
 
 		super.onCreate();
 		
